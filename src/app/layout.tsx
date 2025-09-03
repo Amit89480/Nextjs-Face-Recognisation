f80455ba269186df4e703fa46665996829d6a9cb
Amit Pandey <87856042+Amit89480@users.noreply.github.com>
2025-09-03 23:53:00 +0530

    faceapis completed

---
+++ src/app/layout.tsx
@@ -0,0 +1,19 @@
+import "./globals.css";
+import { Inter } from "next/font/google";
+// import { ThemeProvider } from "next-themes";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        {/* <ThemeProvider attribute="class" defaultTheme="system" enableSystem> */}
+        <div className=" bg-background">
+          <main className="container mx-auto">{children}</main>
+        </div>
+        {/* </ThemeProvider> */}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,41 @@
+import Link from "next/link";
+import { Camera, UserCheck } from "lucide-react";
+
+export default function Home() {
+  const features = [
+    {
+      icon: Camera,
+      title: "Detect Faces",
+      href: "/detect-faces",
+      color: "bg-blue-100 hover:bg-blue-200",
+    },
+    {
+      icon: UserCheck,
+      title: "Face Authentication",
+      href: "/face-auth",
+      color: "bg-red-100 hover:bg-red-200",
+    },
+  ];
+
+  return (
+    <div className="grid grid-cols-2 gap-4 p-4 h-[calc(100dvh-4rem)]">
+      {features.map((feature) => (
+        <Link
+          key={feature.href}
+          href={feature.href}
+          className={`
+            ${feature.color}
+            flex flex-col items-center justify-center
+            rounded-lg shadow-md
+            transition-all duration-300
+            hover:scale-105
+            active:scale-95
+          `}
+        >
+          <feature.icon size={48} className="mb-2" />
+          <span className="text-center font-semibold">{feature.title}</span>
+        </Link>
+      ))}
+    </div>
+  );
+}
